a4625d6c1248ba5fd8333ab5d1b7ccaa30117277
NayemEhsan <naymakon@gmail.com>
2023-11-01 20:56:05 +0600

    first-commit

---
+++ src/components/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import icon
+
+import { BiHomeAlt, BiUser} from 'react-icons/bi';
+import { BsClipboardData, BsBriefcase, BsChatSquare } from 'react-icons/bs';
+
+// import link
+
+import { Link } from 'react-scroll';
+
+const Nav = () => {
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+        <div className="container mx-auto">
+             {/* inner nav */}
+            <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+              <Link className="cursor-pointer w-[60px] h-[60px] flex items-center">
+                   <BiHomeAlt/>  
+              </Link>
+              <Link className="cursor-pointer w-[60px] h-[60px] flex items-center">
+                   <BiUser/>  
+              </Link>
+              <Link className="cursor-pointer w-[60px] h-[60px] flex items-center">
+                   <BsClipboardData/>  
+              </Link>
+              <Link className="cursor-pointer w-[60px] h-[60px] flex items-center">
+                   <BsBriefcase/>  
+              </Link>
+            </div>
+        </div>
+    </nav>
+  );
+};
+
+export default Nav;
